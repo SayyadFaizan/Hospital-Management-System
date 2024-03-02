40e90da2445c55858e65456c73a3709fa326fbdd
SayyadFaizan <141846057+SayyadFaizan@users.noreply.github.com>
2024-03-02 23:13:41 +0530

    Appointment.java

--- src/HospitalManagementSystem/Appointment.java
+++ src/HospitalManagementSystem/Appointment.java
@@ -5,37 +5,41 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
-
+import java.util.InputMismatchException;
 public class Appointment {
     public static void bookAppointment(Patient patient, Doctor doctor, Connection connection, Scanner scanner){
-        System.out.print("Enter Patient Id: ");
-        int patientId = scanner.nextInt();
-        System.out.print("Enter Doctor Id: ");
-        int doctorId = scanner.nextInt();
-        System.out.print("Enter appointment date (YYYY-MM-DD): ");
-        String appointmentDate = scanner.next();
-        if(patient.getPatientById(patientId) && doctor.getDoctorById(doctorId)){
-            if(checkDoctorAvailability(doctorId, appointmentDate, connection)){
-                String appointmentQuery = "INSERT INTO appointments(patient_id, doctor_id, appointment_date) VALUES(?, ?, ?)";
-                try {
-                    PreparedStatement preparedStatement = connection.prepareStatement(appointmentQuery);
-                    preparedStatement.setInt(1, patientId);
-                    preparedStatement.setInt(2, doctorId);
-                    preparedStatement.setString(3, appointmentDate);
-                    int rowsAffected = preparedStatement.executeUpdate();
-                    if(rowsAffected>0){
-                        System.out.println("Appointment Booked!");
-                    }else{
-                        System.out.println("Failed to Book Appointment!");
+        try {
+            System.out.print("Enter Patient Id: ");
+            int patientId = scanner.nextInt();
+            System.out.print("Enter Doctor Id: ");
+            int doctorId = scanner.nextInt();
+            System.out.print("Enter appointment date (YYYY-MM-DD): ");
+            String appointmentDate = scanner.next();
+            if (patient.getPatientById(patientId) && doctor.getDoctorById(doctorId)) {
+                if (checkDoctorAvailability(doctorId, appointmentDate, connection)) {
+                    String appointmentQuery = "INSERT INTO appointments(patient_id, doctor_id, appointment_date) VALUES(?, ?, ?)";
+                    try {
+                        PreparedStatement preparedStatement = connection.prepareStatement(appointmentQuery);
+                        preparedStatement.setInt(1, patientId);
+                        preparedStatement.setInt(2, doctorId);
+                        preparedStatement.setString(3, appointmentDate);
+                        int rowsAffected = preparedStatement.executeUpdate();
+                        if (rowsAffected > 0) {
+                            System.out.println("Appointment Booked!");
+                        } else {
+                            System.out.println("Failed to Book Appointment!");
+                        }
+                    } catch (SQLException e) {
+                        System.out.println(e.getMessage());
                     }
-                }catch (SQLException e){
-                    System.out.println(e.getMessage());
+                } else {
+                    System.out.println("Doctor not available on this date!!");
                 }
-            }else{
-                System.out.println("Doctor not available on this date!!");
+            } else {
+                System.out.println("Either doctor or patient doesn't exist!!!");
             }
-        }else{
-            System.out.println("Either doctor or patient doesn't exist!!!");
+        } catch(InputMismatchException e){
+            System.out.println("Input Mismatch Exception: For Input Int!");
         }
     }
 
